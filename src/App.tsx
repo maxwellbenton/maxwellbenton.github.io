c60469c04ad51ed5644e3a833cd6666dd290b558
Maxwell Benton <maxwellbenton@gmail.com>
2024-07-03 14:35:59 -0400

    feat(project): add features at app level

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
+import { useState } from "react";
 import Header from "./components/Header";
-import { Outlet } from "react-router-dom";
+import Experience from "./pages/Experience";
+import Main from "./pages/Main";
+import Projects from "./pages/Projects";
+// import About from "./pages/About";
+import { Experiment } from "./types";
 
 function App() {
+  const [scrolled, setScrolled] = useState(!!window.scrollY);
+  function updateScrolled() {
+    setScrolled(true);
+  }
+
+  const [experiments, setExperiments] = useState<Experiment[]>([]);
+  
+  function addExperiment({ experiment }: { experiment: Experiment }) {
+    setExperiments([...experiments, { ...experiment }]);
+  }
   return (
-    <div className={`static bg-white h-screen w-screen flex flex-col overflow-y-auto`}>
-      <Header />
-      <Outlet />
+    <div
+      onScroll={updateScrolled}
+      className={`static h-screen w-screen flex flex-col overflow-y-auto overflow-x-hidden`}
+    >
+      <div className={"static w-screen"}>
+        <Header />
+        <Main scrolled={scrolled} addExperiment={addExperiment} experiments={experiments}/>
+      </div>
+      <div className={"static w-screen bg-slate-200"}>
+        <Experience />
+      </div>
+      <div className={"static w-screen bg-slate-200"}>
+        <Projects />
+      </div>
+      {/* <div className={"static w-screen bg-slate-200"}>
+        <About />
+      </div> */}
     </div>
   );
 }
